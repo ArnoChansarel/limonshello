830d0afb587f910c5a0cbfa74116a4a5b25edbe6
Antoine De Bast <ade-bast@shi-r10-p1.s19.be>
2023-04-25 13:43:42 +0200

    good

--- srcs/builtins/ft_pwd.c
+++ srcs/builtins/ft_pwd.c
@@ -6,7 +6,7 @@
 /*   By: ade-bast <ade-bast@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 10:57:01 by ade-bast          #+#    #+#             */
-/*   Updated: 2023/04/25 13:20:37 by ade-bast         ###   ########.fr       */
+/*   Updated: 2023/04/25 13:26:26 by ade-bast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	ft_pwd(t_cmd *cmd)
 		printf("%s\n", buf);
 	else
 		printf("cd: error retrieving current directory: getcwd:\
-		cannot access parent directories: No such file or directory\n");
+cannot access parent directories: No such file or directory\n");
 	return (0);
 }
